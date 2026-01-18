b5f98b45b011b2ea50135a6d695fecffe9423fb1
Meadow Liu <meadowsys@kiwin.gay>
2026-01-18 13:55:39 -0800

    move config and args
    for some reason Visual Studio Code™ Git integration thing is having a really, _really_ hard time showing this diff (as in. it loads literally forever), and this is hilarious to me
    
    ~sf

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,51 @@
+declare const config: Config;
+declare type Config = {
+	readonly runsInApp: boolean;
+	readonly runsInActionExtension: boolean;
+	readonly runsWithSiri: boolean;
+	readonly runsInWidget: boolean;
+	readonly runsInAccessoryWidget: boolean;
+	readonly runsInNotification: boolean;
+	readonly runsFromHomeScreen: boolean;
+	readonly widgetFamily:
+		| "small"
+		| "medium"
+		| "large"
+		| "extraLarge"
+		| "accessoryRectangular"
+		| "accessoryInline"
+		| "accessoryCircular"
+		| null;
+};
+
+declare const args: Args;
+declare type Args = {
+	/**
+	 * @deprecated
+	 */
+	readonly length: number;
+
+	/**
+	 * @deprecated
+	 */
+	readonly all: Array<unknown>;
+
+	readonly plainTexts: Array<string>;
+	readonly urls: Array<string>;
+	readonly fileURLs: Array<string>;
+	readonly images: Array<string>;
+	readonly queryParameters: { [k: string]: string };
+
+	/**
+	 * @deprecated
+	 */
+	readonly siriShortcutArguments: { [k: string]: string };
+
+	readonly shortcutParameter: unknown;
+	readonly widgetParameter: unknown;
+	readonly notification: Notification;
+};
+
 /**
  * Presents an alert
  *
@@ -123,34 +171,6 @@ declare class Alert {
 	presentSheet(): Promise<number>;
 }
 
-declare const args: Args;
-declare type Args = {
-	/**
-	 * @deprecated
-	 */
-	readonly length: number;
-
-	/**
-	 * @deprecated
-	 */
-	readonly all: Array<unknown>;
-
-	readonly plainTexts: Array<string>;
-	readonly urls: Array<string>;
-	readonly fileURLs: Array<string>;
-	readonly images: Array<string>;
-	readonly queryParameters: { [k: string]: string };
-
-	/**
-	 * @deprecated
-	 */
-	readonly siriShortcutArguments: { [k: string]: string };
-
-	readonly shortcutParameter: unknown;
-	readonly widgetParameter: unknown;
-	readonly notification: Notification;
-};
-
 declare class Calendar {
 	static forReminders(): Promise<Array<Calendar>>;
 	static forEvents(): Promise<Array<Calendar>>;
@@ -208,26 +228,6 @@ declare class Color {
 
 	constructor(hex: string, alpha: number);
 }
-
-declare const config: Config;
-declare type Config = {
-	readonly runsInApp: boolean;
-	readonly runsInActionExtension: boolean;
-	readonly runsWithSiri: boolean;
-	readonly runsInWidget: boolean;
-	readonly runsInAccessoryWidget: boolean;
-	readonly runsInNotification: boolean;
-	readonly runsFromHomeScreen: boolean;
-	readonly widgetFamily:
-		| "small"
-		| "medium"
-		| "large"
-		| "extraLarge"
-		| "accessoryRectangular"
-		| "accessoryInline"
-		| "accessoryCircular"
-		| null;
-};
 
 // todo console
 
