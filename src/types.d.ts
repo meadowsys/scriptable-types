9583476101abd610112d897d526c77259cc12aa9
Meadow Liu <meadowsys@kiwin.gay>
2026-01-18 16:10:05 -0800

    Script types
    ~sf

--- src/types.d.ts
+++ src/types.d.ts
@@ -471,7 +471,14 @@ declare class Request {
 
 // todo Safari
 
-// todo Script
+declare class Script {
+	static name(): string;
+	static complete(): void;
+	static setShortcutOutput(value: unknown): void;
+	static setWidget(widget: unknown): void;
+
+	private constructor();
+}
 
 // todo SFSymbol
 
